d12fcb7f875d2e3828a51f945b7f44636071cc2f
Sahil Nagane <sahilnagane07@gmail.com>
2026-04-30 13:04:33 +0530

    fix case issue

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Comment = require("../models/Comments");
+const Comment = require("../models/comments");
 
 
 // ➕ ADD COMMENT
